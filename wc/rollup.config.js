f89b57b093a9097874596bb23ea31135c855a8db
Clement Delafargue <clement@delafargue.name>
2023-04-12 13:11:06 +0200

    Fix datalog editor deployment
    It requires some assets to be copied alongside
    the source code.

--- wc/rollup.config.js
+++ wc/rollup.config.js
@@ -1,5 +1,6 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 
 const sourceDir = 'src';
@@ -15,6 +16,11 @@ export default {
     nodeResolve({ browser: true }),
     commonjs({
       include: 'node_modules/**'
+    }),
+    copy({
+      targets: [
+        { src: "node_modules/@biscuit-auth/web-components/dist/assets/*", dest: "../static/assets" }
+      ],
     }),
     importMetaAssets()
   ]
